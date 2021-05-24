5c669ee7134949f05b008627a9fb5102f74ba054
gifiuza <83991711+gifiuza@users.noreply.github.com>
2021-05-24 20:21:01 -0300

    Add files via upload

---
+++ Propriedades.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Propriedades (props) { 
+    return(
+        <div>
+            <h2>{ props.nome }</h2>
+            <h3>{ props.sobrenome }</h3>
+            <h3>{ props.idade }</h3>
+        </div>
+    ) 
+}
+
+ 
+
+
+
+ 
